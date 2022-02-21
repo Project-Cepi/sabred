4835ed5a562180af4a6a48ce6d13ca6581e5c135
Tristan <LeoDog896@hotmail.com>
2022-02-21 13:29:52 -0500

    Automatically find server jar

--- sabred.ts
+++ sabred.ts
@@ -1,6 +1,39 @@
 import { colors } from "https://deno.land/x/cliffy@v0.20.1/ansi/colors.ts";
+import { basename } from "https://deno.land/std@0.126.0/path/mod.ts";
 
 const rocketEmoji = "🚀"
+const infoEmoji = "❕"
+const errorEmoji = "❌"
+
+const findSabreJar = async (): Promise<string | null> => {	
+
+    let bestVersionScore = 0
+    let chosenFile: string | null = null
+
+    for await (const dirEntry of Deno.readDir(Deno.cwd())) {
+        if (!dirEntry.name.endsWith(".jar")) continue
+
+        // Get the sum of all the character codes
+
+        const score = dirEntry.name.split("").map(it => it.charCodeAt(0)).reduce((a, b) => a + b)
+
+        if (score > bestVersionScore) {
+            bestVersionScore = score
+            chosenFile = dirEntry.name
+        }
+    }
+
+    return chosenFile
+}
+
+const jar = await findSabreJar()
+
+if (jar == null) {
+    console.log(errorEmoji, colors.red(`Could not find any files with .jar`))
+    Deno.exit(0)
+}
+
+console.log(infoEmoji, colors.blue(`Using jar ${basename(jar)}`))
 
 const grabSabreCommand = async (): Promise<Deno.Process> => {
     console.log("")
@@ -14,7 +47,7 @@ const grabSabreCommand = async (): Promise<Deno.Process> => {
     await killCommand.status()
 
     return Deno.run({
-        cmd: ["java", "-Xmx2024M", "-Xms2024M", "-jar", "sabre.jar"],
+        cmd: ["java", "-Xmx2024M", "-Xms2024M", "-jar", jar],
         clearEnv: false
     })
 }
